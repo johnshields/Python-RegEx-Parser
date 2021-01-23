b4869e7ed962399aadf4817bbd786211bf3fe0f3
johnshields <G00348436@gmit.ie>
2021-01-23 20:21:55 +0000

    clean up - readme contents now in wiki

--- regex_parser.py
+++ regex_parser.py
@@ -1,4 +1,4 @@
-# Graph Theory Project
+# RegEx Parser
 # John Shields
 
 # References
@@ -20,75 +20,76 @@
 
 # Shunting Yard Algorithm
 def shunt(infix):
+    # specials dictionary
+    specials = {'*': 50, '.': 40, '|': 30, '+': 40, '?': 35}
 
-   # specials dictionary
-   specials = {'*': 50, '.': 40, '|': 30, '+':40, '?':35}
+    pofix = ""
+    stack = ""
 
-   pofix = ""
-   stack = ""
+    for c in infix:
+        if c == '(':
+            stack = stack + c
+        elif c == ')':
+            while stack[-1] != '(':
+                # putting statements together
+                pofix, stack = pofix + stack[-1], stack[:-1]
+            stack = stack[:-1]
+        elif c in specials:
+            # looks for c in specials dictionary, if not found, give value 0
+            # looks for top of stack in specials dictionary,  if not found, give value 0
+            # n/ compares 1 against the other
+            while stack and specials.get(c, 0) <= specials.get(stack[-1], 0):
+                pofix, stack = pofix + stack[-1], stack[:-1]
+            stack = stack + c
 
-   for c in infix:
-      if c == '(':
-         stack = stack + c
-      elif c == ')':
-         while stack[-1] != '(':
-            # puting statements together
-            pofix, stack = pofix + stack [-1], stack[:-1]
-         stack = stack[:-1]
-      elif c in specials:
-         # looks for c in specials dictionary, if not found, give value 0
-         # looks for top of stack in specials dictionary,  if not found, give value 0
-         # n/ compares 1 against the other
-         while stack and specials.get(c, 0) <= specials.get(stack[-1], 0):
-            pofix, stack = pofix + stack [-1], stack[:-1]
-         stack = stack + c
+        else:
+            pofix = pofix + c
 
-      else:
-         pofix = pofix + c
+    while stack:
+        # puting statements together
+        pofix, stack = pofix + stack[-1], stack[:-1]
 
-   while stack:
-      # puting statements together
-      pofix, stack = pofix + stack [-1], stack[:-1]
- 
-   return pofix
+    return pofix
 
- # Thompson's Construction
- # represents a state with two arrows, labelled by label
- # use 'None' for a label representing 'e' arrows
+
+# Thompson's Construction
+# represents a state with two arrows, labelled by label
+# use 'None' for a label representing 'e' arrows
 class state:
     label = None
     edge1 = None
     edge2 = None
 
+
 # an NFA is represented by its initial and accepts states
-class nfa:    
+class nfa:
     initial = None
     accept = None
 
-    # manatory to have 'self' args in constructor
+    # mandatory to have 'self' args in constructor
     def __init__(self, initial, accept):
         self.initial = initial
         self.accept = accept
 
+
 # Algorithm
 # function has a stack of NFAs
-# pofix allows to loop one character at a time 
+# pofix allows to loop one character at a time
 # until the regular expression is complete
 def compiletom(pofix):
-
     nfastack = []
- 
+
     for c in pofix:
         if c == '.':
-        # stack works as last in first out
-        # method to go to stack 
-        # pop 2 NFAs off the stack.
+            # stack works as last in first out
+            # method to go to stack
+            # pop 2 NFAs off the stack.
             nfa2 = nfastack.pop()
             nfa1 = nfastack.pop()
             # merges them together
             # connect 1st NFAs accept state to the 2nd's initial
             nfa1.accept.edge1 = nfa2.initial
-            
+
             # push NFA to stack
             # one way to do it ¬ nfastack.append(nfa(initial, accept))
             # second way 'easier way'
@@ -128,7 +129,7 @@ def compiletom(pofix):
             initial.edge1 = nfa1.initial
             initial.edge2 = accept
             # join the old accept state to the new accept state and nfa1's initial state.
-            nfa1.accept.edge1 =  nfa.initial
+            nfa1.accept.edge1 = nfa.initial
             nfa1.accept.edge2 = accept
 
             # push new NFA to the stack
@@ -149,7 +150,7 @@ def compiletom(pofix):
             # join the old accept state to the new accept state and nfa1's initial state.
             nfa1.accept.edge1 = nfa1.initial
             nfa1.accept.edge2 = accept
-             # push new NFA to the stack
+            # push new NFA to the stack
             newnfa = nfa(initial, accept)
             # pushes newnfa 
             nfastack.append(newnfa)
@@ -195,14 +196,16 @@ def compiletom(pofix):
 
     # nfastack  should only have a single nfa on it at the point.      
     return nfastack.pop()
-#   
+
+
+#
 # Return the set of states that can be reached from state following e arrows
 def followes(state):
-# create a new set, with state as its only member
+    # create a new set, with state as its only member
     states = set()
     states.add(state)
 
-# check if state has arrows labeled e from it
+    # check if state has arrows labeled e from it
     if state.label is None:
         # Check if edge1 is a state
         if state.edge1 is not None:
@@ -215,6 +218,7 @@ def followes(state):
 
     # return the set of states
     return states
+
 
 # matching
 def match(infix, string):
@@ -246,14 +250,15 @@ def match(infix, string):
         # check if the accept state is in the set of current states
     return (nfa.accept in current)
 
+
 # testing code
 
-print('\n Test Shunting Yard Algortithm \n')      
+print('\n Test Shunting Yard Algorithm \n')
 # test Shunting Yard Algorithm
 print(shunt("(a.b)|(c*.d)"))
 
-print('\n Test Thompsons Construction \n')  
-# test Thompsons Construction
+print('\n Test Thompsons Construction \n')
+# test Thompson's Construction
 print(compiletom("ab.cd.|"))
 print(compiletom("aa.*"))
 print(compiletom("(0|(1(01*(00)*0)*1)*)*"))
@@ -261,23 +266,23 @@ print(compiletom("(0|(1(01*(00)*0)*1)*)*"))
 
 # test Matching
 # creates list of infix regex
-infixes = ["a.b.c" ,"a.b.c*, a.(b|d).c*", "(a.(b|d))*", "a.(b.b)*.c"]
+infixes = ["a.b.c", "a.b.c*, a.(b|d).c*", "(a.(b|d))*", "a.(b.b)*.c"]
 # strings to match against the regex infixes
 strings = ["abc", "abbc", "abcc", "abad", "abbbc"]
 
 print('\n Test Matching \n')
 # double for loop - loops throught the regex and strings
 for exp in infixes:
-  for res in strings:
-      # function prints out True/False if the infix regex matches the string
-      print(match(exp, res), exp, res)
+    for res in strings:
+        # function prints out True/False if the infix regex matches the string
+        print(match(exp, res), exp, res)
 
 # input prompt to match Regular Expressions
 print('\n Input a regular expression to be tested \n')
-    # prints out True/False if the infix regex matches the string
+# prints out True/False if the infix regex matches the string
 while True:
     # writes a infix regex to match with string
-    infixes = (input("Input infix regular expression: ")) 
+    infixes = (input("Input infix regular expression: "))
     # writes a string to match with infix regex
     string = (input("Input string to match: "))
     print('\n Infix and String match \n')
