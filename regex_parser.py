a27eec98a03c7471eca01fcc901f165609d8b1e6
John Shields <johndshields14@gmail.com>
2022-08-17 20:52:54 +0100

    Added bash script.

--- regex_parser.py
+++ regex_parser.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # RegEx Parser
 # John Shields
 
@@ -17,6 +18,8 @@
 # https://stackoverflow.com/questions/70797/user-input-and-command-line-arguments
 # https://stackabuse.com/getting-user-input-in-python/
 # https://stackoverflow.com/questions/3754620/a-basic-question-about-while-true
+import cmd
+
 
 # Shunting Yard Algorithm
 def shunt(infix):
@@ -46,7 +49,7 @@ def shunt(infix):
             pofix = pofix + c
 
     while stack:
-        # puting statements together
+        # putting statements together
         pofix, stack = pofix + stack[-1], stack[:-1]
 
     return pofix
@@ -76,52 +79,52 @@ class nfa:
 # function has a stack of NFAs
 # pofix allows to loop one character at a time
 # until the regular expression is complete
-def compiletom(pofix):
-    nfastack = []
+def compile_tom(pofix):
+    nfa_stack = []
 
     for c in pofix:
         if c == '.':
             # stack works as last in first out
             # method to go to stack
             # pop 2 NFAs off the stack.
-            nfa2 = nfastack.pop()
-            nfa1 = nfastack.pop()
+            nfa2 = nfa_stack.pop()
+            nfa1 = nfa_stack.pop()
             # merges them together
             # connect 1st NFAs accept state to the 2nd's initial
             nfa1.accept.edge1 = nfa2.initial
 
             # push NFA to stack
-            # one way to do it ¬ nfastack.append(nfa(initial, accept))
+            # one way to do it ¬ nfa_stack.append(nfa(initial, accept))
             # second way 'easier way'
-            newnfa = nfa(nfa1.initial, nfa2.accept)
-            # push newnfa 
-            nfastack.append(newnfa)
+            new_nfa = nfa(nfa1.initial, nfa2.accept)
+            # push new_nfa
+            nfa_stack.append(new_nfa)
 
         elif c == '|':
             # pop 2 NFAs off the stack.
-            nfa2 = nfastack.pop()
-            nfa1 = nfastack.pop()
+            nfa2 = nfa_stack.pop()
+            nfa1 = nfa_stack.pop()
             # create a new initial state, connect it to 
             # initial states of the two NFAs popped from the stack
             initial = state()
             initial.edge1 = nfa1.initial
             initial.edge2 = nfa2.initial
             # create new accept state, connecting the accept states 
-            # of the the 2 NFAs popped from the stack, to the new state
+            # of the 2 NFAs popped from the stack, to the new state
             accept = state()
             nfa1.accept.edge1 = accept
             nfa2.accept.edge1 = accept
 
             # push NFA to stack
-            # one way to do it ¬ nfastack.append(nfa(initial, accept))
+            # one way to do it ¬ nfa_stack.append(nfa(initial, accept))
             # second way 'easier way'
-            newnfa = nfa(initial, accept)
-            # push newnfa 
-            nfastack.append(newnfa)
+            new_nfa = nfa(initial, accept)
+            # push new_nfa
+            nfa_stack.append(new_nfa)
 
         elif c == '*':
             # pop a single NFA from the stack
-            nfa1 = nfastack.pop()
+            nfa1 = nfa_stack.pop()
             # create a new initial and accept states
             initial = state()
             accept = state()
@@ -133,15 +136,15 @@ def compiletom(pofix):
             nfa1.accept.edge2 = accept
 
             # push new NFA to the stack
-            # one way to do it ¬ nfastack.append(nfa(initial, accept))
+            # one way to do it ¬ nfa_stack.append(nfa(initial, accept))
             # second way 'easier way'
-            newnfa = nfa(initial, accept)
-            # push newnfa 
-            nfastack.append(newnfa)
+            new_nfa = nfa(initial, accept)
+            # push new_nfa
+            nfa_stack.append(new_nfa)
 
         elif c == '+':
             # pop nfa from stack
-            nfa1 = nfastack.pop()
+            nfa1 = nfa_stack.pop()
             # create a new initial and accept states
             initial = state()
             accept = state()
@@ -151,13 +154,13 @@ def compiletom(pofix):
             nfa1.accept.edge1 = nfa1.initial
             nfa1.accept.edge2 = accept
             # push new NFA to the stack
-            newnfa = nfa(initial, accept)
-            # pushes newnfa 
-            nfastack.append(newnfa)
+            new_nfa = nfa(initial, accept)
+            # pushes new_nfa
+            nfa_stack.append(new_nfa)
 
         elif c == '?':
             # pop nfa from stack
-            nfa1 = nfastack.pop()
+            nfa1 = nfa_stack.pop()
             # create a new initial and accept states
             initial = state()
             accept = state()
@@ -168,9 +171,9 @@ def compiletom(pofix):
             nfa1.accept.edge1 = nfa1.initial
             nfa1.accept.edge2 = accept
             # push new NFA to the stack
-            newnfa = nfa(initial, accept)
-            # pushes newnfa 
-            nfastack.append(newnfa)
+            new_nfa = nfa(initial, accept)
+            # pushes new_nfa
+            nfa_stack.append(new_nfa)
 
         else:
             # create new initial and accept states
@@ -188,104 +191,115 @@ def compiletom(pofix):
             # initial state
 
             # push new NFA to the stack # ¬ returns an instance of the nfa class ¬
-            # one way to do it ¬ nfastack.append(nfa(initial, accept))
+            # one way to do it ¬ nfa_stack.append(nfa(initial, accept))
             # second way 'easier way'
-            newnfa = nfa(initial, accept)
-            # push newnfa 
-            nfastack.append(newnfa)
+            new_nfa = nfa(initial, accept)
+            # push new_nfa
+            nfa_stack.append(new_nfa)
 
-    # nfastack  should only have a single nfa on it at the point.      
-    return nfastack.pop()
+    # nfa_stack  should only have a single nfa on it at the point.
+    return nfa_stack.pop()
 
 
 #
 # Return the set of states that can be reached from state following e arrows
-def followes(state):
+def follow_es(following_state):
     # create a new set, with state as its only member
     states = set()
-    states.add(state)
+    states.add(following_state)
 
     # check if state has arrows labeled e from it
-    if state.label is None:
+    if following_state.label is None:
         # Check if edge1 is a state
-        if state.edge1 is not None:
-            # if theres an edge1, follow it
-            states |= followes(state.edge1)
+        if following_state.edge1 is not None:
+            # if there's an edge1, follow it
+            states |= follow_es(following_state.edge1)
         # check if edge2 is a state
-        if state.edge2 is not None:
-            # if theres an edge2, follow it
-            states |= followes(state.edge2)
+        if following_state.edge2 is not None:
+            # if there's an edge2, follow it
+            states |= follow_es(following_state.edge2)
 
     # return the set of states
     return states
 
 
 # matching
-def match(infix, string):
+def match(infix, string_match):
     # Shunt and compile the regular expression
-    # calls the shunt funciton
+    # calls the shunt function
     postfix = shunt(infix)
-    # calls the compiletom function
-    nfa = compiletom(postfix)
+    # calls the compile_tom function
+    non_fin_auto = compile_tom(postfix)
 
     # current set of states and the next set of the states
     current = set()
-    upcomin = set()
+    upcoming = set()
 
     # add the initial state to the current set
-    current |= followes(nfa.initial)
+    current |= follow_es(non_fin_auto.initial)
 
     # Loop through each character in the string
-    for s in string:
+    for s in string_match:
         # Loop through the current set of states
         for c in current:
             # check of that state is labelled s
             if c.label == s:
-                # Add edge1 state to the upcomin set
-                upcomin |= followes(c.edge1)
-        # set current to upcomin, and clear out next
-        current = upcomin
-        upcomin = set()
+                # Add edge1 state to the upcoming set
+                upcoming |= follow_es(c.edge1)
+        # set current to upcoming, and clear out next
+        current = upcoming
+        upcoming = set()
 
         # check if the accept state is in the set of current states
-    return (nfa.accept in current)
+    return non_fin_auto.accept in current
 
 
 # testing code
+def executioner():
+    print('\nTest Shunting Yard Algorithm \n')
+    # test Shunting Yard Algorithm
+    print(shunt("(a.b)|(c*.d)"))
 
-print('\n Test Shunting Yard Algorithm \n')
-# test Shunting Yard Algorithm
-print(shunt("(a.b)|(c*.d)"))
+    print("\nTest Thompson's Construction \n")
+    # test Thompson's Construction
+    print(compile_tom("ab.cd.|"))
+    print(compile_tom("aa.*"))
+    print(compile_tom("(0|(1(01*(00)*0)*1)*)*"))
+    print(compile_tom("(0|(1(01*(00)*0)*1)*)*"))
 
-print('\n Test Thompsons Construction \n')
-# test Thompson's Construction
-print(compiletom("ab.cd.|"))
-print(compiletom("aa.*"))
-print(compiletom("(0|(1(01*(00)*0)*1)*)*"))
-print(compiletom("(0|(1(01*(00)*0)*1)*)*"))
+    # test Matching
+    # creates list of infix regex
+    infixes = ["a.b.c", "a.b.c*, a.(b|d).c*", "(a.(b|d))*", "a.(b.b)*.c"]
+    # strings to match against the regex infixes
+    strings = ["abc", "abbc", "abcc", "abad", "abbbc"]
 
-# test Matching
-# creates list of infix regex
-infixes = ["a.b.c", "a.b.c*, a.(b|d).c*", "(a.(b|d))*", "a.(b.b)*.c"]
-# strings to match against the regex infixes
-strings = ["abc", "abbc", "abcc", "abad", "abbbc"]
+    print('\n Test Matching \n')
+    # double for loop - loops through the regex and strings
+    for exp in infixes:
+        for res in strings:
+            # function prints out True/False if the infix regex matches the string
+            print(match(exp, res), exp, res)
 
-print('\n Test Matching \n')
-# double for loop - loops throught the regex and strings
-for exp in infixes:
-    for res in strings:
-        # function prints out True/False if the infix regex matches the string
-        print(match(exp, res), exp, res)
+    # input prompt to match Regular Expressions
+    print('\nInput a regular expression to be tested \n')
+    # prints out True/False if the infix regex matches the string
+    while True:
+        try:
+            # writes an infix regex to match with string
+            infixes = (input("Input infix regular expression: "))
+            # writes a string to match with infix regex
+            string = (input("Input string to match: "))
+            print('\nInfix and String match \n')
+            # function prints out True/False if the infix regex matches the string
+            print(match(infixes, string), infixes, string, '\n')
 
-# input prompt to match Regular Expressions
-print('\n Input a regular expression to be tested \n')
-# prints out True/False if the infix regex matches the string
-while True:
-    # writes a infix regex to match with string
-    infixes = (input("Input infix regular expression: "))
-    # writes a string to match with infix regex
-    string = (input("Input string to match: "))
-    print('\n Infix and String match \n')
-    # function prints out True/False if the infix regex matches the string
-    print(match(infixes, string), infixes, string)
-    print('\n')
+            a_file = open("output.txt", "w")
+            text = f'Infix and String match {match(infixes, string), infixes, string} \n'
+            print(text, file=a_file)
+            a_file.close()
+        except (KeyboardInterrupt, EOFError, SystemExit):
+            break
+
+
+if __name__ == "__main__":
+    executioner()
